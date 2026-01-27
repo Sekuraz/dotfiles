45174092b556637cb5d1ed3608ce14b8ca80a952
Markus Baur <markus.baur95@gmail.com>
2026-01-27 13:56:34 +0100

    More tweaks for fish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,10 @@ fish_add_path --path /home/markus/.local/bin /home/markus/.cargo/bin
 # fish setup
 set -g fish_greeting
 set -g fish_prompt_pwd_full_dirs 100
-set -g __fish_git_prompt_use_informative_chars 1
 set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_showupstream informative
+set -g __fish_git_prompt_use_informative_chars 1
+set -g __fish_git_prompt_showstashstate 1
 
 # export computed hostname
 set -x HOSTNAME $hostname
