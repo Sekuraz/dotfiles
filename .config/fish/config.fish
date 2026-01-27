1316e62c48a4e02abec00f07a050cb10816f2fb9
Markus Baur <markusb@selfnet.de>
2026-01-27 03:58:02 +0000

    Update for fish functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,31 +12,6 @@ set -g __fish_git_prompt_show_informative_status 'yes'
 # disble shortening of paths
 set -g fish_prompt_pwd_dir_length 0
 
-# import virtualenvwrapper functions
-command -v python >/dev/null
-and python -m virtualfish compat_aliases >/dev/null ^/dev/null
-and eval (python -m virtualfish compat_aliases)
-
 # start ssh-agent if there is none
 ssh-agent >/dev/null
 
-# attach to screen or start a new one
-if command -v screen >/dev/null ; and not hostname -f | grep login >/dev/null; and set -q SSH_TTY
-    set -l ppid (string trim (ps --pid %self -o ppid=))
-    while true
-        if test $ppid = '1'
-            set -l screenlist (screen -ls | grep 'Attached')
-            and echo -e "Screen is already running and attached:\n $screenlist"
-            or screen -U -R
-            break
-        end
-
-        if string match -q -e 'SCREEN' (ps --pid $ppid -o cmd=)
-            break
-        end
-
-        set -l ppid (string trim (ps --pid $ppid -o ppid=))
-    end
-
-    set -e ppid
-end
