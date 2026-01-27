5678fbe27bf16a4a50bf43040ee7e430a3058f36
Markus Baur <markus.baur95@gmail.com>
2026-01-27 05:21:41 +0100

    clean up of fish config and add ssh config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,8 @@
 
-abbr --add l. 'ls -la'
-abbr --add ll 'ls -l'
-
-set -g EDITOR 'vim'
-
-# show state of git repo
-set -g __fish_git_prompt_showcolorhints 'yes'
-set -g __fish_git_prompt_color_branch 'normal'
-set -g __fish_git_prompt_show_informative_status 'yes'
-
-# disble shortening of paths
-set -g fish_prompt_pwd_dir_length 0
+abbr --add l. 'ls -lah'
+abbr --add ll 'ls -lh'
 
 # start ssh-agent if there is none
 ssh-agent >/dev/null
 
+fish_add_path --path /home/markus/.local/bin /home/markus/.cargo/bin
