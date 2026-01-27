3c948352278068f3259cf163475a1aa177be7e4b
Markus Baur <markus.baur95@gmail.com>
2026-01-27 12:40:54 +0100

    Add grep function

---
+++ .config/fish/functions/grep.fish
@@ -0,0 +1,3 @@
+function grep --description 'alias grep grep -I --color=auto --exclude-dir=Steam --exclude-dir=overlay --exclude-dir=cache'
+    command grep -I --color=auto --exclude-dir=Steam --exclude-dir=overlay --exclude-dir=cache $argv
+end
